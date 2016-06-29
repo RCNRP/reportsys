d07cc256fd8700c53f492f909a610032bbe219b2
zacklogan <bigred9921@mchsi.com>
2016-06-29 09:42:55 -0500

    Update shared.lua

--- shared.lua
+++ shared.lua
@@ -6,4 +6,33 @@ function Killed( victim, inflictor, attacker )
 	end
 end
 hook.Add( "PlayerDeath", "LastKiller", Killed)
+else
+function GetKiller( victim )
+	if killedby[victim] != nil then
+		return killedby[victim]
+	else
+		return "Player hasn't been killed this session"
+	end
+end
+local Frame = vgui.Create( "DFrame" )
+Frame:SetTitle( "Report" )
+Frame:SetSize( 300, 300 )
+Frame:Center()
+Frame:MakePopup()
+Frame.Paint = function( self, w, h ) -- 'function Frame:Paint( w, h )' works too
+	draw.RoundedBox( 0, 0, 0, w, h, Color( 231, 76, 60, 255 ) ) -- Draw a red box instead of the frame
+end
+
+local DComboBox = vgui.Create( "DComboBox" )
+DComboBox:SetPos( 5, 5 )
+DComboBox:SetSize( 100, 20 )
+DComboBox:SetValue( "Choose One" )
+DComboBox:AddChoice( "RDM" )
+DComboBox:AddChoice( "NLR" )
+DComboBox:AddChoice( "RDA" )
+DComboBox:AddChoice( "Other" )
+DComboBox.OnSelect = function( panel, index, value )
+	if (value == "RDM") then
+	  print(GetKiller(self:Nick))
+	end
 end
