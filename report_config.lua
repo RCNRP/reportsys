6c1b5c40ba295fd198734b849549224b2db68a99
zacklogan <bigred9921@mchsi.com>
2016-06-30 09:58:00 -0500

    Create report_config.lua

---
+++ report_config.lua
@@ -0,0 +1,21 @@
+config = {}
+
+config.AdminLocation1 = Vector( 0, 0, 0 )
+config.VictimLocation1 = Vector( 0, 0, 0 )
+config.CulpritLocation1 = Vector( 0, 0, 0 )
+
+config.AdminLocation2 = Vector( 0, 0, 0 )
+config.VictimLocation2 = Vector( 0, 0, 0 )
+config.CulpritLocation2 = Vector( 0, 0, 0 )
+
+config.AdminLocation3 = Vector( 0, 0, 0 )
+config.VictimLocation3 = Vector( 0, 0, 0 )
+config.CulpritLocation3 = Vector( 0, 0, 0 )
+
+config.AdminLocation4 = Vector( 0, 0, 0 )
+config.VictimLocation4 = Vector( 0, 0, 0 )
+config.CulpritLocation4 = Vector( 0, 0, 0 )
+
+config.AdminLocation5 = Vector( 0, 0, 0 )
+config.VictimLocation5 = Vector( 0, 0, 0 )
+config.CulpritLocation5 = Vector( 0, 0, 0 )
